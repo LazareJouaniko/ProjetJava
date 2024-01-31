ff1800e4bf08af913317329955461be0cc64d38a
LazareJouaniko <lazarejouaniko@gmail.com>
2024-01-31 16:38:40 +0100

    lastCommit

--- src/test/java/HearthStoneTest.java
+++ src/test/java/HearthStoneTest.java
@@ -1,16 +1,34 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.example.enums.MonsterTypeEnum;
-import org.example.game.Game;
 import org.example.monsters.Monster;
+import org.example.monsters.MonsterElement;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class HearthStoneTest {
-    Monster monster1 = new Monster(1,"Vampire1",2,2, MonsterTypeEnum.CLASSIC);
-    Monster monster2 = new Monster(1,"Sorcier1",2,2, MonsterTypeEnum.CLASSIC);
+    Monster monster1 = new Monster(1,"Vampire1",2,2, MonsterTypeEnum.CLASSIC, MonsterElement.FIRE);
+    Monster monster2 = new Monster(1,"Sorcier1",2,2, MonsterTypeEnum.CLASSIC,MonsterElement.FIRE);
 
     @Test
     public final void testAttack() {
         monster1.attack(monster2);
         assertEquals(monster2.getHp(),0);
     }
-}
+    @Test
+    public final void testElement(){
+        Monster monster1 = new Monster(0,"Sorcier1",2,2, MonsterTypeEnum.CLASSIC, MonsterElement.FIRE);
+        Monster monster2 = new Monster(1,"Sorcier1",2,2, MonsterTypeEnum.CLASSIC, MonsterElement.WATER);
+        monster1.attack(monster2);
+    }
+    //CHANGELOG :
+    // Monster -> Ajout d'une fonction getWeakness qui retourne un coefficient en fonction de l'adversaire ciblé
+    // Monster -> Attack, ajout du coeff getWeakness dans le calcul des dégats
+    // Ajout d'une enum MonsterElement 
+    // Monster -> Ajout MonsterElement element
+
+
+        
+        
+
+    }
+
